f9f9ee6eab03cf62e50fa7348fa683e474d7a099
Robin <65247410+itSchuleAccRobin@users.noreply.github.com>
2021-07-28 16:06:22 +0200

    Update BoxTheDog.py

--- Segmentation/BoxTheDog.py
+++ Segmentation/BoxTheDog.py
@@ -1,38 +1,58 @@
-from pycocotools.coco import COCO
-import skimage.io as io
+import torchvision
 import matplotlib.pyplot as plt
-import matplotlib
+from PIL import Image
+from torchvision import transforms
+import cv2
 import matplotlib.patches as patches
 
-matplotlib.use('TkAgg')
-dataDir = './COCOdataset2017'
-dataType='val'
-annFile='{}/annotations/instances_{}.json'.format(dataDir,dataType)
-coco=COCO(annFile)
-catIDs = coco.getCatIds()
-cats = coco.loadCats(catIDs)
+model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+model.eval()
+
+COCO_Instance_Category_Names =[  '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',  'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
 
-def getClassName(classID, cats):
-    for i in range(len(cats)):
-        if cats[i]['id']==classID:
-            return cats[i]['name']
-    return "None"
+print("Anzahl Klassen: ",len(COCO_Instance_Category_Names))
 
-filteredClasses = ['cat']
-catIds = coco.getCatIds(catNms=filteredClasses)
-imgIds = coco.getImgIds(catIds=catIds)
+def get_prediction(img_path,threshold=0.5):
+    img = Image.open(img_path)
+    transform = transforms.Compose([transforms.ToTensor()])
+    img = transform(img)
+
+    pred = model([img])
+    pred_score = list(pred[0]['scores'].detach().cpu().numpy())
+    pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1]
+    pred_class = [COCO_Instance_Category_Names[i] for i in list(pred[0]['labels'].cpu().numpy())]
+    pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().cpu().numpy())]
+    pred_boxes = pred_boxes[:pred_t + 1]
+    pred_class = pred_class[:pred_t + 1]
+    return pred_boxes, pred_class
 
 
-#img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
-img = coco.loadImgs(imgIds[10])[0] #SELECTS THE IMAGE
+def instance_segmentation(img_path, threshold=0.5):
+    boxes, pred_cls = get_prediction(img_path, threshold=threshold)
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    counter  = 0
+    petLst= ['bird', 'cat', 'dog', 'horse', 'sheep','cow','elephant', 'bear', 'zebra', 'giraffe']
+    for i in pred_cls:
+        if i not in petLst:
+            counter+=1
+        else:
+            print(pred_cls)
+            fig, ax = plt.subplots()
+            rect = patches.Rectangle((boxes[counter][0][0], boxes[counter][0][1]), boxes[counter][1][0] - boxes[counter][0][0], boxes[counter][1][1] - boxes[counter][0][1], linewidth=3, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+            ax.imshow(img)
+            break
+    return img, pred_cls
 
-I = io.imread('{}/images/{}/{}'.format(dataDir,dataType,img['file_name']))/255.0 #i think it gets the meta data
-
-#plt.axis('off')
-#plt.imshow(I)
-#plt.show()
-
+img, pred_classes = instance_segmentation("F:\PYTHON\Projekte\FindTheDog\Pretrained\img\mensch-vs-hund.jpg")
+plt.show()
 
 fig, ax = plt.subplots() #required -- box
 
