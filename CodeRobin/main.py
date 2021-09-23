db6d0e36a5e1969b01de5ba611e2f8c5ce7afcfc
Robin <65247410+itSchuleAccRobin@users.noreply.github.com>
2021-09-23 17:21:42 +0200

    Update main.py

--- CodeRobin/main.py
+++ CodeRobin/main.py
@@ -1,11 +1,12 @@
 import ImageProcessing.ClassifyPretrained.ClassifyPre
 import ImageProcessing.SegmentationPretrained.fcnResNet
 from ImageProcessing.ColorRecognition import ColorRecognition
-
+from ImageProcessing.CompareImages import compare
 import matplotlib.pyplot as plt
 import cv2
 
-img_path = "F:\PYTHON\Projekte\FindTheDog\ImageProcessing\TestImages\cat.jpg"
+img_path = "F:\PYTHON\Projekte\FindTheDog\ImageProcessing\TestImages\Doberman.jpg"
+imgCropPath = "F:\PYTHON\Projekte\FindTheDog\Crop.png";
 img_raw = cv2.imread(img_path)
 img_raw = cv2.cvtColor(img_raw, cv2.COLOR_BGR2RGB)
 plt.imshow(img_raw)
@@ -20,7 +21,7 @@ plt.imshow(crop_img)
 plt.show()
 
 cv2.imwrite(r'Crop.png', cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB))
-classi = ImageProcessing.ClassifyPretrained.ClassifyPre.PretrainedClassifier("F:\PYTHON\Projekte\FindTheDog\Crop.png")
+classi = ImageProcessing.ClassifyPretrained.ClassifyPre.PretrainedClassifier(imgCropPath)
 classi.classify()
 classi.finalRes()
 print("\n")
@@ -28,4 +29,8 @@ print("\n")
 col = ColorRecognition.ColourInImage()
 print("Dominant"+str(col.closestColor(col.getDominantColour())) + " ==> " + str(col.getDominantColour()))
 print("Average"+ str(col.closestColor(col.getAvgColour())) + " ==> " + str(col.getAvgColour()))
-exit(0)
+
+compareImages = compare.compareImages(img_path,imgCropPath)
+print('\nImages match to '+str(round(float(compareImages.compareVectors()*100),2)).strip("tensor([").strip("])")+"%")
+plt.imshow(crop_img)
+plt.show()
