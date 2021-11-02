2fab75b133b4d460f8aa674d6f83b692b0770611
Robin <65247410+itSchuleAccRobin@users.noreply.github.com>
2021-11-02 13:32:13 +0100

    Update main.py

--- CodeRobin/main.py
+++ CodeRobin/main.py
@@ -2,35 +2,55 @@ import ImageProcessing.ClassifyPretrained.ClassifyPre
 import ImageProcessing.SegmentationPretrained.fcnResNet
 from ImageProcessing.ColorRecognition import ColorRecognition
 from ImageProcessing.CompareImages import compare
-import matplotlib.pyplot as plt
+from Scraping import FindeFixScraper
 import cv2
 
-img_path = "F:\PYTHON\Projekte\FindTheDog\ImageProcessing\TestImages\Doberman.jpg"
+
+imgPath = "F:\PYTHON\Projekte\FindTheDog\ImageProcessing\TestImages\cat.jpg"
 imgCropPath = "F:\PYTHON\Projekte\FindTheDog\Crop.png";
-img_raw = cv2.imread(img_path)
-img_raw = cv2.cvtColor(img_raw, cv2.COLOR_BGR2RGB)
-plt.imshow(img_raw)
-plt.show()
 
-segmi = ImageProcessing.SegmentationPretrained.fcnResNet.SegmentationPretrained(img_path)
-img_box, pet, boxLst, lstIndex = segmi.instance_segmentation()
-print("Pet: ", pet)
-plt.show()
-crop_img = img_raw[int(boxLst[lstIndex][0][1]):int(boxLst[lstIndex][1][1]), int(boxLst[lstIndex][0][0]):int(boxLst[lstIndex][1][0])]
-plt.imshow(crop_img)
-plt.show()
 
-cv2.imwrite(r'Crop.png', cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB))
-classi = ImageProcessing.ClassifyPretrained.ClassifyPre.PretrainedClassifier(imgCropPath)
-classi.classify()
-classi.finalRes()
-print("\n")
+findeFix = FindeFixScraper.FindeFix()
+findeFix.scrapePets(1)
+lstPets = findeFix.getPetLst()
 
-col = ColorRecognition.ColourInImage()
-print("Dominant"+str(col.closestColor(col.getDominantColour())) + " ==> " + str(col.getDominantColour()))
-print("Average"+ str(col.closestColor(col.getAvgColour())) + " ==> " + str(col.getAvgColour()))
+def processInputImage(imgPath):
+    cropImage(imgPath)
 
-compareImages = compare.compareImages(img_path,imgCropPath)
-print('\nImages match to '+str(round(float(compareImages.compareVectors()*100),2)).strip("tensor([").strip("])")+"%")
-plt.imshow(crop_img)
-plt.show()
+def processAll(lstPets):
+    for pet in lstPets:
+        try:
+            cropImage(pet.getImagePath())
+            compareImages(pet.getImagePath())
+            print(pet.outAll())
+        except:
+            print("Fehler bei einem Bild")
+
+def cropImage(imgPath="Crop.png"):
+    imgRaw = cv2.imread(imgPath)
+    imgRaw = cv2.cvtColor(imgRaw, cv2.COLOR_BGR2RGB)
+    segmi = ImageProcessing.SegmentationPretrained.fcnResNet.SegmentationPretrained(imgPath)
+    imgBox, pet, boxLst, lstIndex = segmi.instance_segmentation()
+    #print("Pet: ", pet)
+    cropImg = imgRaw[int(boxLst[lstIndex][0][1]):int(boxLst[lstIndex][1][1]), int(boxLst[lstIndex][0][0]):int(boxLst[lstIndex][1][0])]
+    cv2.imwrite(imgPath, cv2.cvtColor(cropImg, cv2.COLOR_BGR2RGB))
+
+    classifyImage()
+
+def classifyImage():
+    classi = ImageProcessing.ClassifyPretrained.ClassifyPre.PretrainedClassifier(imgCropPath)
+    classi.classify()
+    classi.finalRes()
+
+
+def getColourStuff():
+    col = ColorRecognition.ColourInImage()
+    print("Dominant"+str(col.closestColor(col.getDominantColour())) + " ==> " + str(col.getDominantColour()))
+    print("Average"+ str(col.closestColor(col.getAvgColour())) + " ==> " + str(col.getAvgColour()))
+
+def compareImages(imgPath):
+    compareImages = compare.compareImages("Crop.png",imgPath)
+    print('\nImages match to '+str(round(float(compareImages.compareVectors()*100),2)).strip("tensor([").strip("])")+"%")
+
+processInputImage(imgPath)
+processAll(lstPets)
