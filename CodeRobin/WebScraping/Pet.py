232a64800a3aa7b24ef97db6b28b27ba2bfffd2c
Robin <65247410+itSchuleAccRobin@users.noreply.github.com>
2021-11-02 13:33:28 +0100

    Update Pet.py

--- CodeRobin/WebScraping/Pet.py
+++ CodeRobin/WebScraping/Pet.py
@@ -1,18 +1,19 @@
 class Pet:
-    from PIL import Image
     animal = ""
     name = ""
     place = ""
     dateOfLoss = ""
     findeFixPlakatNr = ""
-    imagePet = ""
+    imagePetPath = "F:\PYTHON\Projekte\FindTheDog\Scraping\\"
     def __init__(self,animal,name,place,dateOfLoss,findeFixPlakatNr,pathImage):
         self.animal = animal
         self.name = name
         self.place = place
         self.dateOfLoss = dateOfLoss
         self.findeFixPlakatNr = findeFixPlakatNr
-        self.imagePet = self.Image.open(pathImage)
+        self.imagePetPath += pathImage
 
     def outAll(self):
-        print(self.animal,self.name,self.place,self.dateOfLoss,self.findeFixPlakatNr)
+        print(self.animal,self.name,self.place,self.dateOfLoss,self.findeFixPlakatNr, self.imagePetPath)
+    def getImagePath(self):
+        return self.imagePetPath
